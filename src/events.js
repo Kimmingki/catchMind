387906235d52ecaef0944198a20af69f3ab539ea
김민기 <rlaalsrl9569@naver.com>
2022-06-16 16:12:37 +0900

    connect & disconnect 처리 및 디자인 수정

--- src/events.js
+++ src/events.js
@@ -1,6 +1,8 @@
 const events = {
   setNickName: "setNickName",
   newUser: "newUser",
+  disconnect: "disconnect",
+  disconnected: "disconnected",
 };
 
 export default events;
--- src/socketController.js
+++ src/socketController.js
@@ -1,9 +1,14 @@
 import events from "./events";
 
 const socketController = (socket) => {
+  const broadcast = (event, data) => socket.broadcast.emit(event, data);
+
   socket.on(events.setNickName, ({ nickname }) => {
-    socket.broadcast.emit(events.newUser, { nickname });
     socket.nickname = nickname;
+    broadcast(events.newUser, { nickname });
+  });
+  socket.on(events.disconnect, () => {
+    broadcast(events.disconnected, { nickname: socket.nickname });
   });
 };
 
